a823d20e32dec0d44c948984673ae4a5fbb1cb73
Henry Alberto Hernandez Martinez <heahernandezma@unal.edu.co>
2021-11-19 17:22:32 -0500

    nov 19

---
+++ Servicios-telematicos/Clase_2/Ejemplo_2/Servidor.js
@@ -0,0 +1,18 @@
+//Importe el módulos
+const express = require("express");
+const app = express();
+const puerto = 5000;
+//Motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/views');
+//Atención de la solicitud
+app.get('/', (req,res)=>{
+    res.render("index", {titulo:"Titulo dinámico.",
+                        parrafo:"Este es un parráfo de ejemplo.",
+                        Variable_n:"Esta es la n-ésima variable."})
+});
+//Oyente
+app.listen(puerto, () => {
+    console.log("Ejecutando express");
+});
+
